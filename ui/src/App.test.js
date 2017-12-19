d8396419ab74a4f3293d6d634579a048b574c325
gayana <gayan.kavindra06@gmail.com>
2017-12-19 20:46:29 +0530

    Update frontend unit tests

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import App from './App';
+import Client from "./Client";
+
+jest.mock('../src/Client');
+
+describe('App tests', () => {
+
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('getSummary function should called exactly once', () => {
+    expect(Client.getSummary.mock.calls.length).toBe(1);
+  });
 });
