67addfd310853f1be052bb5ce05248c1e1005e54
Domantas Petrauskas <dom.petrauskas@gmail.com>
2022-04-18 15:02:14 +0300

    Fix frontend code style check running on Unix systems.

--- project/FrontendCommands.scala
+++ project/FrontendCommands.scala
@@ -3,8 +3,10 @@
   */
 object FrontendCommands {
   val dependencyInstall: String = "yarn install"
-  val codeStyle: String = "npx prettier --write . && npx eslint --max-warnings 0 --fix ."
-  val codeStyleCheck: String = "npx prettier --check . && npx eslint --max-warnings 0 ."
+  val prettier: String = "npx prettier --write ."
+  val prettierCheck: String = "npx prettier --check ."
+  val eslint = "npx eslint --max-warnings 0 --fix ."
+  val eslintCheck = "npx eslint --max-warnings 0 ."
   val test: String = "npm run test"
   val serve: String = "npm run start"
   val build: String = "npm run build"
