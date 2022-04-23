6b051ccd2fc91e9bca4080e5049075a5a9e7283b
Domantas Petrauskas <dom.petrauskas@gmail.com>
2022-04-23 20:23:06 +0300

    Fix tests.

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,20 +1,30 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { act } from 'react-dom/test-utils';
 
 import App from "./App";
 import Client from "./Client";
 
-jest.mock("../src/Client");
+globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
-describe("App tests", () => {
-  it("renders without crashing", () => {
+const render = () => {
+  act(() => {
     const div = document.createElement("div");
     const root = createRoot(div);
     root.render(<App />);
-    root.unmount();
+  });
+}
+
+jest.mock("../src/Client");
+
+describe("App tests", () => {
+
+  it("renders without crashing", () => {
+    render()
   });
 
   it("getSummary function should called exactly once", () => {
+    render()
     expect(Client.getSummary.mock.calls.length).toBe(1);
   });
 });
