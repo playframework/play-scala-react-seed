42164950cb6336fc716e7bf18adfb27054c45ac1
Pirasath Vallipuram <pirasathv@gmail.com>
2024-03-21 22:47:35 -0400

    Add icon

--- ui/src/components/Sidebar.js
+++ ui/src/components/Sidebar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faTimes, faPhone, faEnvelope, faGlobe, faMapMarkedAlt} from "@fortawesome/free-solid-svg-icons";
+import { faFacebookF, faInstagram, faTwitter, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import logo from '../assets/images/logo/logo-1.png';
 
 const Sidebar = () => {
   return (
     <div id="side-bar" className="side-bar">
-      <button className="close-icon-menu"><i className="far fa-times"></i></button>
+      <button className="close-icon-menu"><i className="far"> <FontAwesomeIcon icon={faTimes} /></i></button>
       <div className="rts-sidebar-menu-desktop">
         <a className="logo-1" href="index.html"><img className="logo" src={logo} alt="axela_logo" /></a>
         <div className="body d-none d-xl-block">
@@ -13,29 +16,29 @@ const Sidebar = () => {
           </p>
           <div className="social-wrapper-two">
             <ul className="social-area">
-              <li data-sal-delay="150" data-sal="slide-up" data-sal-duration="800" className="sal-animate"><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-              <li data-sal-delay="250" data-sal="slide-up" data-sal-duration="800" className="sal-animate"><a href="#"><i className="fab fa-twitter"></i></a></li>
-              <li data-sal-delay="350" data-sal="slide-up" data-sal-duration="800" className="sal-animate"><a href="#"><i className="fab fa-linkedin-in"></i></a></li>
-              <li data-sal-delay="450" data-sal="slide-up" data-sal-duration="800" className="sal-animate"><a href="#"><i className="fab fa-skype"></i></a></li>
+              <li data-sal-delay="150" data-sal="slide-up" data-sal-duration="800" className="sal-animate"><a href="#"><i className="fab"><FontAwesomeIcon icon={faFacebookF} /></i></a></li>
+              <li data-sal-delay="250" data-sal="slide-up" data-sal-duration="800" className="sal-animate"><a href="#"><i className="fab"><FontAwesomeIcon icon={faInstagram} /></i></a></li>
+              <li data-sal-delay="350" data-sal="slide-up" data-sal-duration="800" className="sal-animate"><a href="#"><i className="fab"><FontAwesomeIcon icon={faTwitter} /></i></a></li>
+              <li data-sal-delay="450" data-sal="slide-up" data-sal-duration="800" className="sal-animate"><a href="#"><i className="fab"><FontAwesomeIcon icon={faLinkedin} /></i></a></li>
             </ul>
           </div>
           <div className="get-in-touch mt--50">
             <div className="h6 title">Get In Touch</div>
             <div className="wrapper">
               <div className="single">
-                <i className="fas fa-phone-alt"></i>
+                <i className="fas"><FontAwesomeIcon icon={faPhone} /></i>
                 <a href="#">+8801234566789</a>
               </div>
               <div className="single">
-                <i className="fas fa-envelope"></i>
+                <i className="fas"><FontAwesomeIcon icon={faEnvelope} /></i>
                 <a href="#">example@gmail.com</a>
               </div>
               <div className="single">
-                <i className="fas fa-globe"></i>
+                <i className="fas"><FontAwesomeIcon icon={faGlobe} /></i>
                 <a href="#">www.webexample.com</a>
               </div>
               <div className="single">
-                <i className="fas fa-map-marker-alt"></i>
+                <i className="fas"><FontAwesomeIcon icon={faMapMarkedAlt} /></i>
                 <a href="#">13/A, New Pro State, NYC</a>
               </div>
             </div>
